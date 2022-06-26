735f443aa62f06a2ed9d66148edde0e9a9cac1c9
jesusmirena <jesusamh16@gmail.com>
2022-06-25 19:00:50 -0500

    Creating portfolio views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "home.html", context)
