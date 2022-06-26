de0469c3cc374b1d60b6d47e8760d0971df4581a
jesusmirena <jesusamh16@gmail.com>
2022-06-25 19:03:47 -0500

    Creating blog's views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
-# Create your views here.
+
+def render_posts(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "posts.html", context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {"post": post}
+    return render(request, "post_detail.html", context)
